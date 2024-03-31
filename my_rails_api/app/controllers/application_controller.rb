274dead8f4f991b52b1009bf125cfeb6183499d2
Gianluca Romeo <gianlucaromeo@outlook.com>
2024-04-01 00:57:49 +0200

    Fixed application controller

--- my_rails_api/app/controllers/application_controller.rb
+++ my_rails_api/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # Every controller will inherit from this class.
 class ApplicationController < ActionController::API
 
+    skip_before_action :check_token_authorization, only: [:root]
     before_action :check_token_authorization
     rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
 
